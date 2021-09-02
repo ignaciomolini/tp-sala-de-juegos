a5f55a5c836ebc903af679c4b9fca826b3b909b3
Ignacio Molini <ignacio_molini@hotmail.com>
2021-09-02 17:00:37 -0300

    primer commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { QuienSoyComponent } from './quien-soy/quien-soy.component';
+
+const routes: Routes = [
+  {
+    path: "login",
+    component: LoginComponent
+  },
+  {
+    path: "home",
+    component: HomeComponent
+  },
+  {
+    path: "quien-soy",
+    component: QuienSoyComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
